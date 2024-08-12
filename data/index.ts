881d36fc8d031e6e4e045b5a0611c0225e191280
DerrickChen <derrickchen0915@gmail.com>
2024-08-12 12:21:29 -0400

    finish customize

--- data/index.ts
+++ data/index.ts
@@ -71,35 +71,35 @@ export const gridItems = [
 export const projects = [
 	{
 		id: 1,
-		title: "3D Solar System Planets to Explore",
-		des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-		img: "/p1.svg",
-		iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-		link: "https://github.com/adrianhajdin?tab=repositories",
+		title: "Korsmet - A Next-Generation B2B Platform",
+		des: "Korsmet.ca is built with a focus on enhancing efficiency, fostering strong business relationships, and providing a seamless user experience.",
+		img: "/p6.svg",
+		iconLists: ["/next.svg", "/laravel.svg"],
+		link: "https://korsmet.com/",
 	},
 	{
 		id: 2,
-		title: "Yoom - Video Conferencing App",
-		des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-		img: "/p2.svg",
-		iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-		link: "https://github.com/adrianhajdin/zoom-clone",
+		title: "Global Education - Your Trusted Information Hub",
+		des: "gelicanada.com/ is designed to be a user-friendly resource, offering insightful content and up-to-date information across various topics.",
+		img: "/p5.svg",
+		iconLists: ["/next.svg", "/tail.svg"],
+		link: "https://gelicanada.com/",
 	},
 	{
 		id: 3,
-		title: "AI Image SaaS - Canva Application",
-		des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-		img: "/p3.svg",
-		iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-		link: "https://github.com/adrianhajdin/ai_saas_app",
+		title: "HotCityFitness.com - Streamlining Fitness Bookings",
+		des: "HotCityFitness.com is focused on providing a seamless and user-friendly experience, enabling users to easily book their preferred fitness services.",
+		img: "/p7.svg",
+		iconLists: ["/re.svg", "/tail.svg", "/laravel.svg"],
+		link: "https://www.hotcityfitness.com/",
 	},
 	{
 		id: 4,
-		title: "Animated Apple Iphone 3D Website",
-		des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-		img: "/p4.svg",
-		iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-		link: "https://github.com/adrianhajdin/iphone",
+		title: "Promptopia - AI-Powered Prompts",
+		des: "Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts",
+		img: "/p8.svg",
+		iconLists: ["/next.svg", "/tail.svg", "/ts.svg"],
+		link: "https://github.com/DerrickWei/promptopia",
 	},
 ];
 
@@ -172,29 +172,29 @@ export const companies = [
 export const workExperience = [
 	{
 		id: 1,
-		title: "Frontend Engineer Intern",
-		desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+		title: "Web Developer - Aurora Tech Development Inc.",
+		desc: "Responsible for the website project and API developing used for mobile application.",
 		className: "md:col-span-2",
 		thumbnail: "/exp1.svg",
 	},
 	{
 		id: 2,
-		title: "Mobile App Dev - JSM Tech",
-		desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+		title: "Web Developer - ITCD Canada Inc.",
+		desc: "Responsible for the Omini-Cart system(B2B with B2C sites) design and development by using Opencart e-commerce framework",
 		className: "md:col-span-2", // change to md:col-span-2
 		thumbnail: "/exp2.svg",
 	},
 	{
 		id: 3,
-		title: "Freelance App Dev Project",
-		desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+		title: "System Analyst - 99h1 Inc.",
+		desc: "Responsible for Microsoft Dynamics 365 and Business Central Implementation and Customization.",
 		className: "md:col-span-2", // change to md:col-span-2
 		thumbnail: "/exp3.svg",
 	},
 	{
 		id: 4,
-		title: "Lead Frontend Developer",
-		desc: "Developed and maintained user-facing features using modern frontend technologies.",
+		title: "Senior PHP developer",
+		desc: "Responsible for supply chain and block chain platform development by using Laravel and Laravel Nova framework.",
 		className: "md:col-span-2",
 		thumbnail: "/exp4.svg",
 	},
@@ -204,13 +204,11 @@ export const socialMedia = [
 	{
 		id: 1,
 		img: "/git.svg",
+		link: "https://github.com/DerrickWei"
 	},
 	{
 		id: 2,
-		img: "/twit.svg",
-	},
-	{
-		id: 3,
 		img: "/link.svg",
+		link: "https://www.linkedin.com/in/derrick-chen-496797a0/"
 	},
 ];
