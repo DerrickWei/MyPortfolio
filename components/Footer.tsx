5d70b2bd7793df787ade5b8f660bb43a15f6ec4c
DerrickChen <derrickchen0915@gmail.com>
2024-08-12 11:10:42 -0400

    add link to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import { socialMedia } from '@/data'
 
 const Footer = () => {
 	return (
-		<footer className='w-full pt-20 pb-10' id="contact">
+		<footer className='w-full pt-20 pb-5' id="contact">
 			<div className="w-full absolute left-0 -bottom-72 min-h-96">
 				<img 
 					src="/footer-grid.svg"
@@ -38,12 +38,13 @@ const Footer = () => {
 
 				<div className="flex items-center md:gap-3 gap-6">
 					{socialMedia.map((info) => (
-						<div
+						<a
+							href={info.link}
 							key={info.id}
 							className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
 						>
 							<img src={info.img} alt="icons" width={20} height={20} />
-						</div>
+						</a>
 					))}
 				</div>	
 			</div>
