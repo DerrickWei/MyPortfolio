1257b9145da08ced46e7da0394d6f99ed1f0abb7
DerrickChen <derrickchen0915@gmail.com>
2024-08-12 11:10:22 -0400

    remove unused code

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -3,7 +3,6 @@ import React from "react";
 
 import { AnimatePresence, motion } from "framer-motion";
 import { CanvasRevealEffect } from "@/components/ui/CanvasRevealEffect";
-import exp from "constants";
 
 const Approach = () => {
 	return (
